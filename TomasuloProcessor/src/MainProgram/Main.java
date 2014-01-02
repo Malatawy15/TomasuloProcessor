40affaad0aca4f7ceb0989997632b7a6d8aabda8
Ahmad Soliman <ahmadadel922gmail.com>
2014-01-02 22:28:15 +0200

    added more reservation stations

--- TomasuloProcessor/src/MainProgram/Main.java
+++ TomasuloProcessor/src/MainProgram/Main.java
@@ -28,7 +28,10 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		RegisterFile regFile = new RegisterFile();
-
+		
+		System.out.println("Welcome to Tomasulo Simulator, press enter to start entering your program:");
+		br.readLine();
+		
 		// Load Memory Hierarchy
 
 		Memory dataMemory = loadMemorySpecs(br, "L1 data");
